1394ba291e9717185c00316b731ede349a77f132
humbi-exe <humberto.becerra95@gmail.com>
2020-11-30 16:14:50 -0400

    done

--- src/main.js
+++ src/main.js
@@ -5,6 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let randomSuit = generateRandomSuit();
+  document.querySelector(".top").classList.add(randomSuit);
+  document.querySelector(".bot").classList.add(randomSuit);
+  document.querySelector(".mid").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
